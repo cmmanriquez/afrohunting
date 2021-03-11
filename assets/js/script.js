dde4a88ef963b6a9970e00d1cb36329d6c2824c0
Britany Spencergit <britany.spencer@gmail.com>
2021-03-11 19:30:09 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+    $('#toTopBtn').fadeIn();
+    } else {
+    $('#toTopBtn').fadeOut();
+    }
+    });
+
+    $('#toTopBtn').click(function() {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 1000);
+    return false;
+    });
+    });
+
+$('.carousel').carousel({
+  interval: 1990
+})
